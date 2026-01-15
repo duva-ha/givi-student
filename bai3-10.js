1307145c8581e6dbcea94c9f980c076835c06de5
duva-ha <dvhai.vi@gmail.com>
2026-01-15 19:24:46 +0700

    Update bai3-10.js

--- bai3-10.js
+++ bai3-10.js
@@ -1,6 +1,5 @@
 window.D10_B3 = {
     id: "D10-B3", // ID phải duy nhất
-    grade: "10",
     title: "Bài 3: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
     content: "Nội dung bài 3...",
     instruction: "Hướng dẫn học bài 3..."
