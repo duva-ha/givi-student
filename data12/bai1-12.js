d591b0cbf267a7e226e6794816d55f9c00d1d289
duva-ha <dvhai.vi@gmail.com>
2026-01-15 10:17:51 +0700

    Create bai1-12.js

---
+++ data12/bai1-12.js
@@ -0,0 +1,7 @@
+// data12/bai1-12.js
+const D12_B1 = { 
+    id: "12-1", 
+    title: "Bài 1: Nhập môn Công nghệ 12", 
+    content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
+    instruction: "Hoàn thành bài tập trắc nghiệm cuối bài." 
+};
