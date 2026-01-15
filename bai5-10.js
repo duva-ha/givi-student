046fee58f4e96a7643ec15312801852a48b5c96d
duva-ha <dvhai.vi@gmail.com>
2026-01-15 22:07:05 +0700

    Update bai5-10.js

--- bai5-10.js
+++ bai5-10.js
@@ -1,6 +1,6 @@
 window.D10_B5 = {
     id: "D10-B5", // ID phải duy nhất
-    title: "Bài 4: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    title: "Bài 5: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
     content: "Nội dung bài 5...",
     instruction: "Hướng dẫn học bài 5..."
 };
