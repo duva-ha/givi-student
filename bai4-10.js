9d1170ad2c38d2cd33b2e02cc7aae635fb493159
duva-ha <dvhai.vi@gmail.com>
2026-01-15 19:25:01 +0700

    Update bai4-10.js

--- bai4-10.js
+++ bai4-10.js
@@ -1,6 +1,5 @@
 window.D10_B4 = {
     id: "D10-B4", // ID phải duy nhất
-    grade: "10",
     title: "Bài 4: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
     content: "Nội dung bài 3...",
     instruction: "Hướng dẫn học bài 3..."
