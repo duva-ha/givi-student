3e931fdd89f91faae9689c5afef7dd28837dd1f6
duva-ha <dvhai.vi@gmail.com>
2026-01-15 22:17:13 +0700

    Create bai18-10.js

---
+++ bai18-10.js
@@ -0,0 +1,6 @@
+window.D10_B18 = {
+    id: "D10-B18", // ID phải duy nhất
+    title: "Bài 18: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    content: "Nội dung bài 5...",
+    instruction: "Hướng dẫn học bài 5..."
+};
