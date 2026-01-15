0e4b0c930d6d47199a9540bb6b488ff0dadbc5cb
duva-ha <dvhai.vi@gmail.com>
2026-01-15 19:10:17 +0700

    Update bai2-10.js

--- bai2-10.js
+++ bai2-10.js
@@ -1,7 +1,7 @@
 // data10/bai2-10.js
 window.D10_B2 = { 
     id: "10-2", 
-    title: "Bài 1: Nhập môn Công nghệ", 
+    title: "Bài 2: Nhập môn Công nghệ", 
     content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
     instruction: "Hoàn thành bài tập trắc nghiệm cuối bài." 
 };
