224d56b5bea4a140c2c69b3a79a1ac56d74e5f55
duva-ha <dvhai.vi@gmail.com>
2026-01-15 14:22:13 +0700

    Update bai1-12.js

--- data12/bai1-12.js
+++ data12/bai1-12.js
@@ -1,5 +1,5 @@
 // data12/bai1-12.js
-const D12_B1 = { 
+window.D12_B1 = { 
     id: "12-1", 
     title: "Bài 1: Nhập môn Công nghệ 12", 
     content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
