c2b0bbad66c842a77912429d71671cfebb2a0a29
duva-ha <dvhai.vi@gmail.com>
2026-01-15 22:16:37 +0700

    Create bai17-10.js

---
+++ bai17-10.js
@@ -0,0 +1,6 @@
+window.D10_B17 = {
+    id: "D10-B17", // ID phải duy nhất
+    title: "Bài 17: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    content: "Nội dung bài 5...",
+    instruction: "Hướng dẫn học bài 5..."
+};
