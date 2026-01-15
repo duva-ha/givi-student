6630a47dfdffcda2098080e753e502e521abe7a9
duva-ha <dvhai.vi@gmail.com>
2026-01-15 22:13:48 +0700

    Create bai15-10.js

---
+++ bai15-10.js
@@ -0,0 +1,6 @@
+window.D10_B15 = {
+    id: "D10-B15", // ID phải duy nhất
+    title: "Bài 15: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    content: "Nội dung bài 15...",
+    instruction: "Hướng dẫn học bài 5..."
+};
