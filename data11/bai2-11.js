0911140229ee66adb6fa3056f9776340d50d3a2a
duva-ha <dvhai.vi@gmail.com>
2026-01-15 10:04:11 +0700

    Create bai2-11.js

---
+++ data11/bai2-11.js
@@ -0,0 +1,7 @@
+// data11/bai2-11.js
+const D12_B2 = { 
+    id: "11-2", 
+    title: "Bài 2: Nhập môn Công nghệ", 
+    content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
+    instruction: "Hoàn thành bài tập trắc nghiệm cuối bài." 
+};
