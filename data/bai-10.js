51d47809e5a9f36f7b73474b772eea95cae7922e
duva-ha <dvhai.vi@gmail.com>
2026-01-15 10:00:17 +0700

    Delete data/bai-10.js

--- data/bai-10.js
+++
@@ -1,7 +0,0 @@
-// data10/bai1-10.js
-const D10_B1 = { 
-    id: "10-1", 
-    title: "Bài 1: Nhập môn Công nghệ", 
-    content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
-    instruction: "Hoàn thành bài tập trắc nghiệm cuối bài." 
-};
