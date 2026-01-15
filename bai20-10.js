9cd8c76d66552afab4470f8f17f554d840753cf3
duva-ha <dvhai.vi@gmail.com>
2026-01-15 22:31:44 +0700

    Create bai20-10.js

---
+++ bai20-10.js
@@ -0,0 +1,6 @@
+window.D10_B20 = {
+    id: "D10-B20", // ID phải duy nhất
+    title: "Bài 20: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    content: "Nội dung bài 20...",
+    instruction: "Hướng dẫn học bài 20..."
+};
