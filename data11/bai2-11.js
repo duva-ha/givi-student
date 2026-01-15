922cee7fcdba6a247cad0d9623f8cb29673869e5
duva-ha <dvhai.vi@gmail.com>
2026-01-15 10:31:45 +0700

    Update bai2-11.js

--- data11/bai2-11.js
+++ data11/bai2-11.js
@@ -1,5 +1,5 @@
 // data11/bai2-11.js
-const D12_B2 = { 
+const D11_B2 = { 
     id: "11-2", 
     title: "Bài 2: Nhập môn Công nghệ", 
     content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
