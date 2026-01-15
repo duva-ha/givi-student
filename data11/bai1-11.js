a02feb574ef2f189cae0915e7c3764c3b7320307
duva-ha <dvhai.vi@gmail.com>
2026-01-15 10:03:19 +0700

    Create bai1-11.js

---
+++ data11/bai1-11.js
@@ -0,0 +1,7 @@
+// data10/bai1-11.js
+const D11_B1 = { 
+    id: "11-1", 
+    title: "Bài 1: Nhập môn Công nghệ", 
+    content: "Khoa học nghiên cứu quy luật tự nhiên, Công nghệ ứng dụng chúng vào đời sống...", 
+    instruction: "Hoàn thành bài tập trắc nghiệm cuối bài." 
+};
