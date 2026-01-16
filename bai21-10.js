f82904d908564ddae97ea28257d4029b26a22a2f
duva-ha <dvhai.vi@gmail.com>
2026-01-16 11:40:49 +0700

    Create bai21-10.js

---
+++ bai21-10.js
@@ -0,0 +1,6 @@
+window.D10_B21 = {
+    id: "D10-B21", // ID phải duy nhất
+    title: "Bài 21: Một số công nghệ phổ biến", // Sửa lại tiêu đề tại đây
+    content: "Nội dung bài 20...",
+    instruction: "Hướng dẫn học bài 20..."
+};
