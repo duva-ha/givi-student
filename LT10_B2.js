7687e8508af2c6593d1407e09a893b14dee08a5e
duva-ha <dvhai.vi@gmail.com>
2026-01-16 21:45:09 +0700

    Update and rename LTAP2.js to LT10_B2.js

---
+++ LT10_B2.js
@@ -0,0 +1,14 @@
+// File: LT10_B2.js
+const LT10_B2 = [
+    { 
+        q: "Thiết bị nào sau đây là đầu vào của hệ thống kỹ thuật?", 
+        a: ["Cảm biến", "Động cơ", "Bóng đèn", "Loa"], 
+        c: 0 // Vị trí đáp án đúng (0 là câu đầu tiên)
+    },
+    { 
+       q: "Vật liệu nào sau đây là kim loại đen?", 
+       a: ["Gang", "Đồng đỏ", "Nhôm kẽm", "Hợp kim titan"],
+       c: "Gang" }, 
+       
+    }
+]; 
