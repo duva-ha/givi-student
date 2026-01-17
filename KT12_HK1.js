c023bdceae21cf6a9e4de95659607efc49296c83
duva-ha <dvhai.vi@gmail.com>
2026-01-17 10:00:55 +0700

    Create KT12_HK1.js

---
+++ KT12_HK1.js
@@ -0,0 +1,8 @@
+window.KT12_HK1 = [
+    {
+        q: "Câu 1 (HK1): Vai trò của bản vẽ kỹ thuật trong đời sống là gì?",
+        a: ["Để trang trí", "Để làm tài liệu học tập", "Để thiết kế và chế tạo", "Cả 3 đáp án trên"],
+        c: 3
+    },
+    // Thầy dán thêm các câu khác của HK1 vào đây...
+];
