7d08457b83e029b91aeecb0d3d0d8161b24d36fd
duva-ha <dvhai.vi@gmail.com>
2026-01-17 21:01:37 +0700

    Create LT11_B1.js

---
+++ LT11_B1.js
@@ -0,0 +1,12 @@
+window.LT11_B1 = [
+    {
+        q: "Thiết bị nào sau đây là đầu vào của hệ thống kỹ thuật?",
+        a: ["Cảm biến", "Động cơ", "Bóng đèn", "Loa"],
+        c: 0
+    },
+    {
+        q: "Vật liệu nào sau đây là kim loại đen?",
+        a: ["Gang", "Đồng đỏ", "Nhôm kẽm", "Hợp kim titan"],
+        c: 0
+    }
+];
